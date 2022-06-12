38ecf2901db7b5ba07d82a513e0ac43d84fe3a64
ardylavyn <90450310+ardylavyn@users.noreply.github.com>
2022-06-12 11:48:32 +0700

    Update Semester2UAS.java

--- src/semester2uas/Semester2UAS.java
+++ src/semester2uas/Semester2UAS.java
@@ -5,9 +5,16 @@ public class appToko {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //daftar barang yang dijual
         cBarang brg1 = new cBarang("Tas", 100000);
         cBarang brg2 = new cBarang("Sandal", 150000);
-        cBarang brg3 = new cBarang("Sepatu", 200000);        
+        cBarang brg3 = new cBarang("Sepatu", 200000); 
+        //data member toko
+        int id1 = 111, pwd1 = 123;
+        int id2 = 112, pwd2 = 1234;
+        int id3 = 113, pwd3 = 1235;
+        int id, pin;
+        //daftar antrian transaksi yang masuk toko
         cDaftarTransaksi jual = new cDaftarTransaksi();
         int pilih=0, pilih2=0,pilih3=0;
         int kode=100,jumlah;
@@ -78,6 +85,81 @@ public class appToko {
                     break;
                 case 2:
                     //anggota
+                    boolean valid=false;
+                    do {
+                        System.out.print("ID  = ");
+                        id=sc.nextInt();
+                        System.out.print("PIN = ");
+                        pin=sc.nextInt();
+                        if(id==id1 && pin==pwd1) {
+                            valid=true;
+                        }
+                        else if(id==id2 && pin==pwd2){
+                            valid=true;
+                        }
+                        else if(id==id3 && pin==pwd3){
+                            valid=true;
+                        }
+                        if(valid==false) {
+                            System.out.println("PIN/ID salah!");
+                        }
+                        else break;
+                    } while(valid==false);
+                    if (valid==true) {
+                        System.out.println("Selamat Datang...");
+                        kode++;
+                        beli = new cDaftarTransaksi();
+                        do{
+                            System.out.println("Akun Member");
+                            System.out.println("1.Tambah");
+                            System.out.println("2.Hapus");
+                            System.out.println("3.Lihat");
+                            System.out.println("4.Kembali");
+                            System.out.print("Pilih = ");
+                            pilih2 = sc.nextInt();
+                            switch(pilih2){
+                                case 1 :
+                                    cTransaksi br = null;
+                                    System.out.println("Daftar Barang");
+                                    System.out.println("1.Tas");
+                                    System.out.println("2.Sandal");
+                                    System.out.println("3.Sepatu");   
+                                    System.out.print("Pilih = ");
+                                    pilih3 = sc.nextInt();
+                                    System.out.print("Jumlah = ");
+                                    jumlah =  sc.nextInt();
+                                    if (pilih3==1) {
+                                        br = new cTransaksi(String.valueOf(kode), String.valueOf(id), brg1, jumlah, 0);
+                                    }
+                                    else if (pilih3==2) {
+                                        br = new cTransaksi(String.valueOf(kode), String.valueOf(id), brg2, jumlah, 0);
+                                    }
+                                    else if (pilih3==3) {
+                                        br = new cTransaksi(String.valueOf(kode), String.valueOf(id), brg3, jumlah, 0);
+                                    }   
+                                    beli.tambahTransaksi(br);
+                                    break;
+                                case 2 :
+                                    //hapus transaksi
+                                    beli.lihatTransaksi();
+                                    System.out.print("Hapus Nomor = ");
+                                    int hapus = sc.nextInt( );
+                                    beli.hapusTransaksi(hapus);
+                                    break;
+                                case 3 :
+                                    //menampilkan daftar belanja dan diskon^
+                                    beli.lihatTransaksiMember();
+                                    break;
+                                case 4 :
+                                    //selesi. sambungkan transaksi pembeli
+                                    //ke antrian toko
+                                    jual.sambungTransaksi(beli.getFront(), beli.getRear());
+                                    System.out.println("Selamat datang kembali..");
+                                    break;                                
+                            }
+                        }while(pilih2!=4);
+                    break; 
+                    }
                     break;
                 case 3:
                     //admin
@@ -108,7 +190,8 @@ public class appToko {
                     break;
                 case 4:
                     //pemilik
-                    //melihat embelian yang sudah dproses
+                    //melihat pembelian yang sudah dproses
+                    System.out.println("Akun Pemilik");
                     System.out.println("Transaksi Diproses : "+jual.lihatDiproses());
                     System.out.println("Pemasukan          : "+jual.lihatPemasukan());
                     break;
